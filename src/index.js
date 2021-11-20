8f450d9def0c5dedb3d18499d1cf18fb6fc143f3
x4sq <ryan110507@icloud.com>
2021-11-20 02:40:43 -0600

    First commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { Collection, Client, Discord } = require('discord.js');
+const client = new Client({
+    disableEveryone: true
+})
+
+const mongoose  = require('mongoose')
+mongoose.connect(require('./config.json').mongo, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}).then(console.log('Connected to mongo db!'));
+
+const path = require('path')
+const fs = require('fs')
+const config = require('./config.json');
+module.exports = client;
+client.commands = new Collection();
+client.prefix = config.prefix;
+client.aliases = new Collection();
+client.categories = fs.readdirSync(path.resolve('src/commands'));
+["command"].forEach(handler => {
+    require(path.resolve(`src/handlers/${handler}`))(client);
+}); 
+
+
+process.on("unhandledRejection", (reason, p) => {
+    console.log(" [antiCrash] :: Unhandled Rejection/Catch");
+    console.log(reason, p);
+});
+process.on("uncaughtException", (err, origin) => {
+    console.log(" [antiCrash] :: Uncaught Exception/Catch");
+    console.log(err, origin);
+});
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+    console.log(" [antiCrash] :: Uncaught Exception/Catch (MONITOR)");
+    console.log(err, origin);
+});
+process.on("multipleResolves", (type, promise, reason) => {
+    console.log(" [antiCrash] :: Multiple Resolves");
+    console.log(type, promise, reason);
+});
+
+client.login(config.token);
